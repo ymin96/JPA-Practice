968c706e2968288b2a4eac5b262c7cfdec1bef2d
Yong Hwan Kim <ymin96@naver.com>
2020-09-21 00:49:36 +0900

    jpa init

--- src/main/java/com/ymin/jpa/config/DBConfig.java
+++ src/main/java/com/ymin/jpa/config/DBConfig.java
@@ -1,18 +1,48 @@
 package com.ymin.jpa.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 public class DBConfig {
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(){
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        factory.setJpaProperties(properties());
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("com.ymin.jpa");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    private Properties properties(){
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.use_sql_comments", "true");
+        properties.put("hibernate.id.new_generator_mappings", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create");
+        return properties;
+    }
 
     @Bean(name="dataSource")
     @ConfigurationProperties(prefix="spring.datasource.hikari")
@@ -21,8 +51,9 @@ public class DBConfig {
     }
 
     @Bean
-    public PlatformTransactionManager txManager()
-    {
-        return new DataSourceTransactionManager(dataSource());
+    public PlatformTransactionManager txManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory());
+        return txManager;
     }
 }
